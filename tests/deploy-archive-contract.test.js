2f2c94ea3714c10a4d4a1fe833bb9164a42d3064
Kamatbot <kamatbot@Kamatbots-Mac-mini.local>
2026-08-28 12:10:12 +0700

    Exclude native sources from web deploy archives

---
+++ tests/deploy-archive-contract.test.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const test = require("node:test");
+const assert = require("node:assert/strict");
+const fs = require("node:fs");
+const path = require("node:path");
+
+const script = fs.readFileSync(path.join(__dirname, "../scripts/pack-deploy.sh"), "utf8");
+
+test("web deployment archive excludes native iOS sources in every format", () => {
+  const exclusions = script.match(/git ls-files[^\n]*':\(exclude\)ios\/\*\*'/g) || [];
+  assert.equal(exclusions.length, 2, "zip and tar packaging must both exclude ios/**");
+});
